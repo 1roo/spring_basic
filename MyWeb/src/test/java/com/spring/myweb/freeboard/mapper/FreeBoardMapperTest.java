a51778b2c4ea6aeb0b08315181e126dcf01ff313
1roo <93hanaru@gmail.com>
2023-05-22 14:34:39 +0900

    20230519(금) 언규꺼

---
+++ MyWeb/src/test/java/com/spring/myweb/freeboard/mapper/FreeBoardMapperTest.java
@@ -0,0 +1,132 @@
+package com.spring.myweb.freeboard.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.spring.myweb.command.FreeBoardVO;
+import com.spring.myweb.util.PageVO;
+
+// 테스트 환경을 만들어 주는 junit5 객체 로딩
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = {
+		"file:src/main/webapp/WEB-INF/config/db-config.xml"
+})
+public class FreeBoardMapperTest {
+	
+	@Autowired
+	private IFreeBoardMapper mapper;
+	
+	// 단위 테스트 (unit test) - 가장 작은 단위의 테스트
+	// 테스트 시나리오
+	// - 단언 (Assertion) 기법
+	
+	@Test
+	@DisplayName("Mapper 계층의 regist를 호출하면서 "
+			+ "FreeBoardVO를 전달하면 데이터가 INSERT 된다.")
+	void registTest() {
+		// given - when - then 패턴을 따릅니다. (생략 가능)
+		
+		// given : 테스트를 위해 주어질 데이터 (ex : parameter)
+		for(int i=1; i<=200; i++) {
+			FreeBoardVO vo = new FreeBoardVO();
+			vo.setTitle("테스트 제목 " + i);
+			vo.setWriter("abc1234");
+			vo.setContent("테스트 내용입니다. " + i);			
+			// when : 테스트 실제 상황
+			mapper.regist(vo);
+		}
+		
+		// then : 테스트 결과를 확인.
+		
+	}
+	
+	@Test
+	// 전체 글 목록을 조회하고, 조회된 글 갯수를 파악했을 때 하나가 조회될 것이다.
+	@DisplayName("사용자가 원하는 페이지 번호에 맞는 글 목록을 불러올 것이고, "
+			+ "게시물의 개수는 사용자가 원하는 만큼의 개수를 가진다.")
+	void getListTest() {
+		
+		PageVO vo = new PageVO();
+		vo.setPageNum(7);
+		
+		List<FreeBoardVO> list = mapper.getList(vo);
+		
+		/*
+		for(FreeBoardVO article : list) {
+			System.out.println(article);
+		}
+		*/
+		
+		list.forEach(article -> System.out.println(article));
+		
+		assertEquals(vo.getCpp(), list.size());
+		
+	}
+	
+	@Test
+	@DisplayName("글 번호가 2번인 글을 조회하면 "
+			+ "글쓴이는 'abc1234'일 것이고 글 내용은 '메롱메롱' 일 것이다.")
+	void getContentTest() {
+		
+		// given
+		int bno = 2;
+		
+		// when
+		FreeBoardVO vo = mapper.getContent(bno);
+		
+		// then
+		assertEquals("abc1234", vo.getWriter());
+		assertEquals("메롱메롱", vo.getContent());
+//		assertTrue(vo.getContent().equals("메롱메롱"));
+//		assertNull(vo);
+		
+	}
+	
+	@Test
+	@DisplayName("글 번호가 1번인 글의 제목과 내용을 수정 후 다시 조회했을 때 "
+			+ "제목이 수정한 제목으로 바뀌었음을 단언한다.")
+	void updateTest() {
+		
+		// given
+		FreeBoardVO vo = null;
+		vo.setBno(1);
+		vo.setTitle("수정수정");
+		vo.setContent("수정한 내용입니다.");
+		
+		// when
+		mapper.update(vo);
+		
+		// then
+		assertEquals(vo.getTitle(), mapper.getContent(vo.getBno()).getTitle());
+		
+	}
+	
+	@Test
+	@DisplayName("글 번호가 2번인 글을 삭제한 후에 리스트를 전체 조회했을 때 "
+			+ "글의 개수는 1개일 것이고, "
+			+ "2번 글을 조회했을 때 null이 반환되어야 한다.")
+	void deleteTest() {
+		
+		// given
+		int bno = 2;
+		
+		// when
+		mapper.delete(bno);
+		
+		// then
+//		assertEquals(1, mapper.getList().size());
+		assertNull(mapper.getContent(bno));
+		
+	}
+
+}
---
+++ SpringDBAccess/src/main/java/com/spring/db/service/ScoreService.java
@@ -0,0 +1,41 @@
+package com.spring.db.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.db.model.ScoreVO;
+import com.spring.db.repository.IScoreMapper;
+
+@Service
+public class ScoreService implements IScoreService {
+
+	@Autowired
+	private IScoreMapper mapper;
+	
+	@Override
+	public void insertScore(ScoreVO vo) {
+		vo.calcData();
+		System.out.println("service : " + vo);
+		mapper.insertScore(vo);
+	}
+
+	@Override
+	public List<ScoreVO> selectAllScores() {
+		return mapper.selectAllScores();
+	}
+
+	@Override
+	public void deleteScore(int stuId) {
+		mapper.deleteScore(stuId);
+		System.out.println("삭제 메서드 실행 종료!");
+		return;
+	}
+
+	@Override
+	public ScoreVO selectOne(int stuId) {
+		return mapper.selectOne(stuId);
+	}
+
+}
